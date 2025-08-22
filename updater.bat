9eb0d0d92c814ee41477c75c14b91b12c0e79d62
Paulo Bennertz <paulobennertz@gmail.com>
2025-08-22 03:18:27 -0300

    atualização endereco api

---
+++ updater.bat
@@ -0,0 +1,17 @@
+
+@echo off
+echo Aguardando o aplicativo fechar...
+taskkill /F /PID 23060 > nul 2>&1
+timeout /t 2 /nobreak > nul
+
+echo Substituindo arquivos...
+move /Y "D:\Importantes\MonitorCriptomoedas3.2.1\venv\Scripts\python.exe" "D:\Importantes\MonitorCriptomoedas3.2.1\python.old"
+move /Y "D:\Importantes\MonitorCriptomoedas3.2.1\update.exe" "D:\Importantes\MonitorCriptomoedas3.2.1\venv\Scripts\python.exe"
+
+echo Limpando...
+del "D:\Importantes\MonitorCriptomoedas3.2.1\python.old" > nul 2>&1
+
+echo Reiniciando o aplicativo...
+start "" "D:\Importantes\MonitorCriptomoedas3.2.1\venv\Scripts\python.exe"
+
+(goto) 2>nul & del "%~f0"
